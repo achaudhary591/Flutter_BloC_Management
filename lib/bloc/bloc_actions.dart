b6ac786a2cacfc8b49e2b6672b03592f88a26aa8
achaudhary591 <achaudhary591@gmail.com>
2023-03-24 21:06:25 +0530

    tessting

--- lib/bloc/bloc_actions.dart
+++ lib/bloc/bloc_actions.dart
@@ -1,9 +1,8 @@
-
 import 'package:bloc_state_management/bloc/person.dart';
 import 'package:flutter/foundation.dart' show immutable;
 
-const persons1Url = 'http://127.0.0.1:5500/api/persons1.json';
-const persons2Url = 'http://127.0.0.1:5500/api/persons2.json';
+const persons1Url = 'http://10.0.2.2:8080/api/persons1.json';
+const persons2Url = 'http://10.0.2.2:8080/api/persons2.json';
 
 typedef PersonsLoader = Future<Iterable<Person>> Function(String url);
 
